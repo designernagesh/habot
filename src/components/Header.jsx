47fed6428377b5bb54c024e5197866e0b0c081bc
designernagesh <designer.nagesh@gmail.com>
2024-10-30 01:09:45 +0530

    header alighment changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -11,10 +11,10 @@ export default function Header() {
     <>
       <nav className="bg-white fixed w-full z-20 top-0 start-0">
         <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-          <a href="https://habot.io/" className="flex items-center space-x-3 rtl:space-x-reverse">
+          <a href="https://habot.io/" className="flex items-center">
             <img src={habotLogo} className="h-8" alt="Habot Logo" />
           </a>
-          <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
+          <div className="flex md:order-2 items-center">
             <button type="button" className="text-green hover:text-white border border-green hover:bg-green font-bold text-base rounded-md px-8 py-4 text-center">Login / Sign Up</button>
             <button onClick={menuHandler} data-collapse-toggle="navbar-sticky" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm rounded-lg md:hidden" aria-controls="navbar-sticky" aria-expanded="false">
               <span className="sr-only">Open main menu</span>
@@ -24,7 +24,7 @@ export default function Header() {
             </button>
           </div>
 
-          <div className={`${active && 'hidden'} items-center grow justify-end md:mr-7 w-full md:flex md:w-auto md:order-1 p-6 md:p-0`} id="navbar-sticky">
+          <div className={`${active === true ? '' : 'hidden'} items-center grow justify-end md:mr-7 w-full md:flex md:w-auto md:order-1 p-6 md:p-0`} id="navbar-sticky">
             <ul className="flex flex-col md:flex-row gap-8">
               <li>
                 <a href="#" className="block text-base text-grey">Find Suppliers</a>
